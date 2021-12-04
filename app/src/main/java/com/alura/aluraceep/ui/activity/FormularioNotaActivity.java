f14c00f18843b9bda4deb882bf930a29e71f17ec
geninhocell <geninhocell@gmail.com>
2021-12-04 02:12:13 -0300

    envio de objeto entre activities

--- app/src/main/java/com/alura/aluraceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/com/alura/aluraceep/ui/activity/FormularioNotaActivity.java
@@ -3,6 +3,7 @@ package com.alura.aluraceep.ui.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,11 +30,14 @@ public class FormularioNotaActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
-        if(itemId == R.id.menu_formulario_nota_ic_salva){
+        if (itemId == R.id.menu_formulario_nota_ic_salva) {
             EditText titulo = findViewById(R.id.formulario_nota_titulo);
             EditText descricao = findViewById(R.id.formulario_nota_descricao);
             Nota notaCriada = new Nota(titulo.getText().toString(), descricao.getText().toString());
-            new NotaDAO().insere(notaCriada);
+
+            Intent resultadoInsercao = new Intent();
+            resultadoInsercao.putExtra("nota", notaCriada);
+            setResult(2, resultadoInsercao);
             finish();
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/alura/aluraceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/alura/aluraceep/ui/activity/ListaNotasActivity.java
@@ -1,14 +1,13 @@
 package com.alura.aluraceep.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.alura.aluraceep.R;
 import com.alura.aluraceep.ui.dao.NotaDAO;
@@ -30,28 +29,31 @@ public class ListaNotasActivity extends AppCompatActivity {
         todasNotas = notasDeExemplo();
         configuraRecyclerView(todasNotas);
 
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == 2) {
+                        Intent data = result.getData();
+                        Nota notaRecebida = (Nota) data.getSerializableExtra("nota");
+                        new NotaDAO().insere(notaRecebida);
+                        adapter.adiciona(notaRecebida);
+                    }
+                });
+
         TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
         botaoInsereNota.setOnClickListener(v -> {
             Intent intent = new Intent(this, FormularioNotaActivity.class);
-            startActivity(intent);
+            someActivityResultLauncher.launch(intent);
         });
     }
 
-    @Override
-    protected void onResume() {
-        NotaDAO notaDAO = new NotaDAO();
-        todasNotas.clear();
-        todasNotas.addAll(notaDAO.todos());
-        adapter.notifyDataSetChanged();
-        super.onResume();
-    }
 
     private List<Nota> notasDeExemplo() {
         NotaDAO notaDAO = new NotaDAO();
 //        for (int i = 0; i < 10000; i++) {
 //            notaDAO.insere(new Nota("Nota " + i, "descrição " + i));
 //        }
-        notaDAO.insere(new Nota("Primeira Nota", "Primeira descrição" ));
+        notaDAO.insere(new Nota("Primeira Nota", "Primeira descrição"));
         notaDAO.insere(new Nota("Segunda Nota", "Segunda descrição"));
         List<Nota> todasNotas = notaDAO.todos();
         return todasNotas;
